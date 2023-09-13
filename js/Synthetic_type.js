e1cd0791d1b1c62f73ff1f561ac2a4ebcf9deb93
bopin <49940777+bopin2020@users.noreply.github.com>
2023-09-13 15:14:54 +0800

    Update Synthetic_type.js

--- js/Synthetic_type.js
+++ js/Synthetic_type.js
@@ -48,6 +48,47 @@ function exe(cmd)
     return result;
 }
 
+/*
+Synthetic type  shortcoming:
+1. 结构体中定义函数指针无法识别
+2. typedef 定义别名类型使用不了
+
+0:000> dx -r2 Debugger.Utility.Analysis.SyntheticTypes.CreateInstance("_gcrypt_cipher_handle",@$addr)
+Error: Unable to get property 'size' of undefined or null reference [at SynTypes (line 102 col 5)]
+
+Eek! We hit an error.
+https://lowleveldesign.wordpress.com/2019/08/27/synthetic-types-and-tracing-syscalls-in-windbg/
+
+Windbg无法计算某些我们自定义结构的size
+*/
+
+> 通过迭代展开定位到哪一个成员windbg 解析fault        
+```
+0:000> dx -r1 Debugger.Utility.Analysis.SyntheticTypes.ReadHeader("C:\\Users\\admin\\Desktop\\gcry.h","ntdll").Types[5].Description
+Debugger.Utility.Analysis.SyntheticTypes.ReadHeader("C:\\Users\\admin\\Desktop\\gcry.h","ntdll").Types[5].Description                
+    [0x0]            : int magic
+    [0x1]            : size_t actual_handle_size
+    [0x2]            : size_t handle_offset
+    [0x3]            : void* spec
+    [0x4]            : int algo
+    [0x5]            : cipher_mode_ops mode_ops
+    [0x6]            : cipher_bulk_ops bulk
+    [0x7]            : int mode
+    [0x8]            : unsigned int flags
+    [0x9]            : __UNNAMED_TYPE_0 marks
+    [0xa]            : __UNNAMED_TYPE_1 u_iv
+    [0xb]            : __UNNAMED_TYPE_2 u_ctr
+    [0xc]            : unsigned char[16] lastiv
+    [0xd]            : int unused
+0:000> dx -r1 Debugger.Utility.Analysis.SyntheticTypes.ReadHeader("C:\\Users\\admin\\Desktop\\gcry.h","ntdll").Types[5].Description[1]
+Debugger.Utility.Analysis.SyntheticTypes.ReadHeader("C:\\Users\\admin\\Desktop\\gcry.h","ntdll").Types[5].Description[1]                 : size_t actual_handle_size
+    Size             : Unable to get property 'size' of undefined or null reference [at SynTypes (line 102 col 5)]
+    Align            : Unable to get property 'align' of undefined or null reference [at SynTypes (line 65 col 5)]
+```
+
+> 修正之后发现   Debugger.Utility.Analysis.SyntheticTypes.TypeTables  不能Clear
+>     
+
 
 function parse(structpath,structname,addr){
     output("start :)\n");
